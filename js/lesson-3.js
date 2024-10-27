d37985be51dd3639d6f695ea8837c0cef6b72340
Dima <oxxTerm0s@gmail.com>
2024-10-27 15:06:32 +0200

    task-2

--- js/lesson-3.js
+++ js/lesson-3.js
@@ -57,11 +57,46 @@ const tweets = [
 // console.log(getNames(tweets));
 
 
-function getRandomValues(array, prop) {
+// function getRandomValues(array, prop) {
 
-    if (!array[0][prop]) {
-        return "Sory, no such property in array!";
+//     if (!array[0][prop]) {
+//         return "Sory, no such property in array!";
+//     }
+//     return array.map(item => item[prop]);
+// }
+// console.log(getRandomValues(tweets, "gende"));
+
+
+// 2. Напишіть функцію getUsersWithJs(array), яка приймає масив об'єктів і повертає масив тільки тих користувачів,
+// у кого є тег "js" (властивість tags)
+
+// function getUsersWithJs(array) {
+//   return array.filter(item => item.tags.includes("js") )
+
+// }
+// console.log(getUsersWithJs(tweets));
+
+
+// 3. Написати функцію getUsersWithGender(array, gender), яка приймає масив і стать
+// і повертає масив імен користувачів по цій статі (властивість gender)
+
+// function getUsersWithGender(array, gender) {
+//   return array.filter(item => item.gender === gender)
+//     .map(item => item.name)
+  
+// }
+
+function getUsersWithGender(array, gender) {
+  return array.reduce((names, item) => {
+    if (item.gender === gender) {
+      names.push(item.name)
     }
-    return array.map(item => item[prop]);
+    return names
+  }, [])
 }
-console.log(getRandomValues(tweets, "gende"));
+
+
+
+console.log(getUsersWithGender(tweets, "male"));
+
+
